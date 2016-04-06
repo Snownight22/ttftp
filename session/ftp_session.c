84f72c77c2c3a97f2d5dfd3427a903762c91e52c
root <root@localhost.(none)>
2016-04-06 17:28:42 +0800

    add ftp session basic files

---
+++ session/ftp_session.c
@@ -0,0 +1,11 @@
+/* FileName:ftp_session.c;
+ *
+ * Author:snownight;
+ * Date:2016.4.6;
+ *
+ */
+#include <stdio.h>
+
+#include "ftp_err.h"
+#include "ftp_session.h"
+
---
+++ session/ftp_session.h
@@ -0,0 +1,10 @@
+/* FileName:ftp_session.h;
+ *
+ * Author:snownight;
+ * Date:2016.4.6;
+ *
+ */
+#ifndef _FTP_SESSION_H_
+#define _FTP_SESSION_H_
+
+#endif
