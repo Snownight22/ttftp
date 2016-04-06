0e10715eccee005413b7b5c11dc319ff54ac61aa
root <root@localhost.(none)>
2016-04-06 17:45:02 +0800

    add ftp command files

---
+++ command/ftp_command.c
@@ -0,0 +1,11 @@
+/* FileName:ftp_command.c;
+ *
+ * Author:snownight;
+ * Date:2016.4.6;
+ *
+ */
+#include <stdio.h>
+
+#include "ftp_err.h"
+#include "ftp_comman.h"
+
---
+++ command/ftp_command.h
@@ -0,0 +1,10 @@
+/* FileName:ftp_command.h;
+ *
+ * Author:snownight;
+ * Date:2016.4.6;
+ *
+ */
+#ifndef _FTP_COMMAND_H_
+#define _FTP_COMMAND_H_
+
+#endif
